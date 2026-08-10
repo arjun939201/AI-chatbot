aba9a0d90511a2e6bc686b6d9d7a51cd48288f7b
arjun939201 <throwuse829@gmail.com>
2026-08-10 11:17:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,8 +2,18 @@ import os
 
 from dotenv import load_dotenv
 
+
 load_dotenv()
 
+
 GROQ_TOKEN = os.getenv("GROQ_TOKEN")
-GROQ_MODEL = os.getenv("GROQ_MODEL")
-GROQ_URL = os.getenv("GROQ_URL")
+
+GROQ_URL = os.getenv(
+    "GROQ_URL",
+    "https://api.groq.com/openai/v1/chat/completions"
+)
+
+GROQ_MODEL = os.getenv(
+    "GROQ_MODEL",
+    "llama-3.3-70b-versatile"
+)
