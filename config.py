10c3bd383277025fb1fe90028e72f4360fe4b780
arjun939201 <throwuse829@gmail.com>
2026-08-10 16:14:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,14 +2,18 @@ import os
 
 from dotenv import load_dotenv
 
+
 load_dotenv()
 
+
 GROQ_TOKEN = os.getenv("GROQ_TOKEN")
+
 
 GROQ_URL = os.getenv(
     "GROQ_URL",
     "https://api.groq.com/openai/v1/chat/completions"
 )
+
 
 GROQ_MODEL = os.getenv(
     "GROQ_MODEL",
